95bc02543528520004f7a49d0a6f1d747bf55dd3
Mikel <mikel.san@educa.madrid.org>
2025-11-14 10:40:03 +0100

    creados ficheros de ejercicios

---
+++ app/src/main/java/org/ies/myapp/FlightScreen.kt
@@ -0,0 +1,14 @@
+package org.ies.myapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+fun FlightScreen(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FlightScreenPreview() {
+    FlightScreen()
+}
---
+++ app/src/main/java/org/ies/myapp/LoginScreen.kt
@@ -0,0 +1,14 @@
+package org.ies.myapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+fun LoginScreen(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
+}
